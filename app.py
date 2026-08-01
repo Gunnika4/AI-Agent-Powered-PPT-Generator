f67d69c21e5da42eab471d5ae28ab196caae1c45
Gunnika Bindra <84628650+Gunnika4@users.noreply.github.com>
2026-08-01 15:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ import numpy as np
 import streamlit as st
 
 # ========= API KEYS ============
-GOOGLE_KEY = st.sidebar.text_input("Google-API",type = "password") 
-GROQ_KEY = st.sidebar.text_input("Groq-API", type = "password")
-TAVILY_KEY = st.sidebar.text_input("Tavily-API", type = "password")
+GOOGLE_API_KEY = st.sidebar.text_input("GEMINI",type = "password") 
+GROQ_API_KEY = st.sidebar.text_input("GROQ", type = "password")
+TAVILY_API_KEY = st.sidebar.text_input("TAVILY", type = "password")
 
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 os.environ["TAVILY_API_KEY"] = TAVILY_API_KEY
 
 
-ALL_API = [GOOGLE_KEY, GROQ_KEY, TAVILY_KEY]
+ALL_API = [GOOGLE_API_KEY, GROQ_API_KEY, TAVILY_API_KEY]
 
 if not all(ALL_API):
   st.sidebar.error("PASS API-KEYS")
